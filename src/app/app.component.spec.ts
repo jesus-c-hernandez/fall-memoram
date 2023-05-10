abb36fa822a0b3065e2806e6f733d73968cb7414
Jesus Saul Hernandez <jesus.castaneda@metricamovil.com>
2023-05-09 23:51:06 -0600

    Update responsive solo-mode 1280 and 1024 screen resolution

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+      });
+  }));
 
-  it(`should have as title 'fall-memoram'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fall-memoram');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fall-memoram app is running!');
+  it('should create the AppComponent', () => {
+    expect(component).toBeTruthy();
   });
 });
--- src/app/modules/main/components/title/title.component.spec.ts
+++ src/app/modules/main/components/title/title.component.spec.ts
@@ -1,23 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TitleComponent } from './title.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('TitleComponent', () => {
+fdescribe('TitleComponent', () => {
   let component: TitleComponent;
   let fixture: ComponentFixture<TitleComponent>;
+  let ele: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TitleComponent ]
-    })
-    .compileComponents();
+      declarations: [TitleComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TitleComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    ele = fixture.debugElement;
   });
 
-  it('should create', () => {
+  it('should create the TitleComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the title of the app', () => {
+    const textFall = 'FALL';
+    const textMemo = 'MEMORAM';
+
+    fixture.detectChanges();
+
+    const mainTitle = ele.queryAll(By.css('.main-title'));
+    const mainTitleSec = ele.queryAll(By.css('.main-title-sec'));
+    const mainTitleFront = ele.queryAll(By.css('.main-title-front'));
+    const img = ele.queryAll(By.css('.img-fall'));
+
+    expect(mainTitle.length).toBeGreaterThan(0);
+    expect(mainTitleSec.length).toBeGreaterThan(0);
+    expect(mainTitleFront.length).toBe(2);
+    expect(img.length).toBeGreaterThan(0);
+
+    expect(mainTitle[0].nativeElement.textContent).toBe(textFall);
+    expect(mainTitleSec[0].nativeElement.textContent).toBe(textMemo);
+    expect(mainTitleFront[0].nativeElement.textContent).toBe(textFall);
+    expect(mainTitleFront[1].nativeElement.textContent).toBe(textMemo);
+    expect(img[0].nativeElement.src).toContain('/assets/img/cara.png');
   });
 });
